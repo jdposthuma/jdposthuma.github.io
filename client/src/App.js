d9da3f7158fe47bb07993723d19a1cb085c81428
Jason Posthuma <jason.posthuma@intterragroup.com>
2018-10-09 18:25:03 -0600

    click works

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './Flip.css';
 
 class App extends Component {
+
+  state = {
+    flipped: false
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.clicked = this.clicked.bind(this);
+  }
+
+  clicked() {
+
+    console.log("Clicked!");
+
+    const newState = {
+      flipped: !this.state.flipped
+    };
+
+    this.setState(newState);
+  }
+
   render() {
+
+
+    const blakeStyle = {
+      backgroundImage: "url(" + "https://d1o51r9qdgnnlz.cloudfront.net/static_image/8fbc4829-7ccd-4cb4-a8ac-182dac6eb7a3/static.jpg" + ")"
+    };
+
     return (
       <div className="App">
         <header className="App-header">
-          <div class="flip-box">
-            <div class="flip-box-inner">
-              <div class="flip-box-front">
-                <img src="https://d1o51r9qdgnnlz.cloudfront.net/static_image/8fbc4829-7ccd-4cb4-a8ac-182dac6eb7a3/static.jpg" alt="Blake"  />
+          <div className={"flip-box" + (this.state.flipped ? " flipped" : "")} onClick={this.clicked}>
+            <div className="flip-box-inner">
+              <div className="flip-box-front" style={blakeStyle}>
               </div>
-              <div class="flip-box-back">
+              <div className="flip-box-back">
                 <h2>blake</h2>
                 <p>hello world!</p>
               </div>
