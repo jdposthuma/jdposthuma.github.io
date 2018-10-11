5b1c787d5412a33f237e157382f5fffdf4ebe4b8
Jason Posthuma <jason.posthuma@intterragroup.com>
2018-10-11 07:55:07 -0600

    more characters

--- client/src/App.js
+++ client/src/App.js
@@ -24,23 +24,49 @@ class App extends Component {
       {
         name: "yang",
         url:
-       "https://vignette.wikia.nocookie.net/rwby/images/4/47/Yang_Vol_5_Profile.png/revision/latest?cb=20171017205551",
+          "https://vignette.wikia.nocookie.net/rwby/images/4/47/Yang_Vol_5_Profile.png/revision/latest?cb=20171017205551",
         flipped: false
 
-      },  
+      },
       {
-      name: "nora",
-      url:
-      "https://vignette.wikia.nocookie.net/rwby/images/0/04/NoraV5Ver2.png/revision/latest?cb=20180113042818"
+        name: "nora",
+        url:
+          "https://vignette.wikia.nocookie.net/rwby/images/0/04/NoraV5Ver2.png/revision/latest?cb=20180113042818"
 
       },
       {
         name: "pyrrha",
         url:
-        "https://vignette.wikia.nocookie.net/rwby/images/4/47/Vol2_Pyrrha_ProfilePic_Uniform.png/revision/latest?cb=20151114141423"
+          "https://vignette.wikia.nocookie.net/rwby/images/4/47/Vol2_Pyrrha_ProfilePic_Uniform.png/revision/latest?cb=20151114141423"
 
+      },
+      {
+        name: "ren",
+        url:
+          "https://vignette.wikia.nocookie.net/rwby/images/8/83/Vol3_Ren_ProfilePic_Normal.png/revision/latest?cb=20160126164317"
+      },
+      {
+        name: "jaune",
+        url:
+          "https://vignette.wikia.nocookie.net/rwby/images/1/18/Jaune_ProfilePic_4.png/revision/latest?cb=20161104220926"
+      },   
+      {
+        name: "cinder",
+        url:
+        "https://vignette.wikia.nocookie.net/rwby/images/2/24/Cinder_ProfilePic_Normal.png/revision/latest?cb=20141223010925"
+      },
+      {
+      name: "qrow",
+      url:
+      "https://pbs.twimg.com/media/CvqLhkkVMAAyAgL.jpg"
+      },
+      {
+      name: "salem",
+      url: "https://cdn.jwplayer.com/thumbs/8rFogOxZ-720.jpg"
+      
       }
-    ]
+      
+      ]
   };
 
   constructor(props) {
