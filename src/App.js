6ebad9d841d09c0f1d06fa2944e8eab821dc2a34
Jason Posthuma <jdposthuma@gmail.com>
2020-07-22 18:41:47 -0600

    added bay's rwby, dad's sao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { list } from './services/data'
+import { games } from './services/data'
 import Card from './components/Card'
 import uuid from 'uuid/v4'
 import './App.css';
@@ -7,12 +7,25 @@ import './Flip.css';
 
 function App() {
 
-  const listWithUuids = list.map(x => Object.assign(x, { key: uuid() })),// eslint-disable-next-line
-    [myList, setMyList] = useState(listWithUuids),
+  for (const key in games) {
+    const game = games[key];
+    game.map(x => Object.assign(x, { key: uuid() }))
+  }
+
+  // eslint-disable-next-line
+  const [myGames, setMyGames] = useState(games),
+    [currentGameName, setCurrentGameName] = useState(null),
+    [currentGame, setCurrentGame] = useState(null),
     [showSplashScreen, setShowSplashScreen] = useState(true);
 
-  function startGame() {
+  function startGame(gameName) {
+    setCurrentGameName(gameName)
+    setCurrentGame(games[gameName])
     setShowSplashScreen(false);
+  }
+
+  function reset() {
+    setShowSplashScreen(true)
   }
 
   function renderSplashScreen() {
@@ -22,7 +35,9 @@ function App() {
           <h1 className="title" >Welcome!</h1>
         </header >
         <main className="App-header" >
-          <button className="cta" onClick={startGame} >Start Game</button>
+          {
+            Object.keys(myGames).map(x => <button className="cta" key={x} onClick={() => startGame(x)} >{x}</button>)
+          }
         </main >
       </div>
     );
@@ -32,10 +47,11 @@ function App() {
     return (
       <div className="App" >
         <header>
-          <h1 className="title">RWBY Guess Who</h1>
+          <h1 className="title">{currentGameName} Guess Who</h1>
+          <button onClick={reset}>Reset</button>
         </header >
         <main className="App-header">
-          {myList.map(x => <Card key={x.key} character={x} />)}
+          {currentGame.map(x => <Card key={x.key} character={x} />)}
         </main>
       </div>
     );
