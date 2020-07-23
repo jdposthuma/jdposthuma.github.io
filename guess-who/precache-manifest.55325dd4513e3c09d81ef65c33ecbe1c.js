783b394b6b9e9ff489eb4fe882f69561cebb09c7
Jason Posthuma <jdposthuma@gmail.com>
2020-07-22 23:20:38 -0600

    initial guess who

---
+++ guess-who/precache-manifest.55325dd4513e3c09d81ef65c33ecbe1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "784d47e616eb76f95053",
+    "url": "/guess-who/static/css/main.6b82dabe.chunk.css"
+  },
+  {
+    "revision": "784d47e616eb76f95053",
+    "url": "/guess-who/static/js/main.784d47e6.chunk.js"
+  },
+  {
+    "revision": "654a7b3b2ff3bd825ea4",
+    "url": "/guess-who/static/js/1.654a7b3b.chunk.js"
+  },
+  {
+    "revision": "54d5c85bc075d61d74ef",
+    "url": "/guess-who/static/js/runtime~main.54d5c85b.js"
+  },
+  {
+    "revision": "3877f06d75ab42d53c7e3f49ec72da3d",
+    "url": "/guess-who/index.html"
+  }
+];
